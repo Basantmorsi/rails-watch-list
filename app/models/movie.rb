dc813deb6b9a4eb842c7a7d99e76c0c1889d3d59
Basant Morsi <basant179@gmail.com>
2023-11-23 12:27:16 +0100

    validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, presence: true ,uniqueness: true
+  validates :overview, presence: true
+  # before_destroy :check_for_bookmarks
+
+  # private
+
+  # def check_for_bookmarks
+  #   if !bookmarks.empty?
+  #     errors.add_to_base("cannot delete movie while it is referenced in at least one bookmark")
+  #     return false
+  #   end
+  # end
 end
