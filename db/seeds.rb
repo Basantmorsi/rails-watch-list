70c15d263d676dc3ff88fc4be44c7c72ccb958c8
Basant Morsi <basant179@gmail.com>
2023-11-23 13:56:00 +0100

    seeding movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+puts "Removing all movies....."
+Movie.destroy_all
+puts "Seeding new data....."
+5.times do
+  Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: Faker::Number.between(from: 0.0, to: 10.0))
+  Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: Faker::Number.between(from: 0.0, to: 10.0))
+  Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: Faker::Number.between(from: 0.0, to: 10.0))
+  Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: Faker::Number.between(from: 0.0, to: 10.0))
+end
+puts "Seeding finished, #{Movie.count} movies are created"
